bbf1b8a6bbf57b658e1f0e38c4cf8cd56bf81824
Miles Grey <benjaminmjin@gmail.com>
2018-06-11 13:08:39 +0200

    add mongoose models

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const hapi = require('hapi');
 const mongoose = require('mongoose');
+const Painting = require('./models/Painting');
 
 const server = hapi.server({
     port: 4000,
@@ -7,11 +8,31 @@ const server = hapi.server({
 });
 
 const init = async () => {
-    server.route({
+    server.route([
+        {
         method: 'GET',
         path: '/',
         handler: (request, reply) => `<h1>My modern api</h1>`
-    });
+        },
+        {
+            method: 'GET',
+            path: '/api/v1/paintings',
+            handler: (req, reply) => Painting.find()
+        },
+        {
+            method: 'POST',
+            path: '/api/v1/paintings',
+            handler: (req, reply) => {
+                const { name, url, techniques } = req.payload;
+                const painting = new Painting({
+                    name,
+                    url,
+                    techniques
+                });
+                return painting.save();
+            }
+        }
+    ]);
 
     await server.start();
     console.log(`Server running at: ${server.info.uri}`);
